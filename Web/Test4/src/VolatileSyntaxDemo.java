8629751805c59e0bc7612f0b29a2a73d7db16ba3
PeterLiblack <812769273@qq.com>
2020-06-18 21:49:39 +0800

    多线程安全

---
+++ Web/Test4/src/VolatileSyntaxDemo.java
@@ -0,0 +1,14 @@
+/**
+ * @Author Li_ZW
+ * @Date: 2020/06/18
+ * @Time: 21:47
+ * @Description
+ */
+public class VolatileSyntaxDemo {
+    /**
+     * volatile 修饰变量的定义
+     * volatile 修饰的是共享的变量（属性 和 静态属性的定义）
+     */
+    volatile int a;
+    static volatile int sa;
+}
